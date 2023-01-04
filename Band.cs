7c72b08a7b90cdfea748e0444f3727256b49275e
Milagro Scardigno <mdscardigno@gmail.com>
2023-01-04 14:29:33 -0500

    Band POCO

--- Band.cs
+++ Band.cs
@@ -2,6 +2,13 @@ namespace RhythmsGonnaGetYou
 {
     public class Band
     {
-
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int CountryOfOrigin { get; set; }
+        public string NumberOfMembers { get; set; }
+        public string Website { get; set; }
+        public string Genre { get; set; }
+        public bool IsSigned { get; set; }
+        public string ContactName { get; set; }
     }
 }
